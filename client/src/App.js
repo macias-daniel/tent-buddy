1634217d36201c095f4ef02a281b56f683b13110
PinkFished <d.macias.apps@gmail.com>
2020-05-15 13:09:53 -0700

    Push start files

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+// Our Components
+import { AuthProvider, useAuth } from "./utils/auth";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+function ProtectedRoute({ children, ...rest }) {
+  const { isLoggedIn } = useAuth();
+  if (isLoggedIn) {
+    return children;
+  }
+  return <Redirect to="/signup" />;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <ProtectedRoute exact path="/">
+              <Home />
+            </ProtectedRoute>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/signup">
+              <Signup />
+            </Route>
+            <ProtectedRoute exact path="/profile">
+              <Profile />
+            </ProtectedRoute>
+          </Switch>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+
+import "./index.css";
+import App from "./App";
+
+import registerServiceWorker from "./registerServiceWorker";
+
+// Here is if we have an id_token in localStorage
+if (localStorage.getItem("id_token")) {
+  // then we will attach it to the headers of each request from react
+  // application via axios
+  axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem("id_token")}`;
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
