7c0d831c24e67db0772e8f0eae4f79630d0c5f34
PinkFished <d.macias.apps@gmail.com>
2020-05-19 12:35:59 -0700

    Update post and get route

--- routes/api.js
+++ routes/api.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const userController = require("../controller/user");
 
 //Get users widgets
-router.get("/api/user/:_id", (req, res) => {
-  const userID = req.params._id;
-  userController.getUserWidgets(userID).then((userData) => {
+router.get("/api/:username", (req, res) => {
+  const username = req.params.username;
+  userController.getUserWidgets(username).then((userData) => {
     res.send(userData);
   });
 });
 
 //Post a new widget to user
 router.post("/api/widget", (req, res) => {
-  const userID = req.body.userID;
+  const username = req.body.username;
   const widgetType = req.body.type;
   const widgetData = req.body.data;
   userController
-    .addUserWidget(userID, widgetType, widgetData)
+    .addUserWidget(username, widgetType, widgetData)
     .then((response) => {
       res.send(response);
     });
