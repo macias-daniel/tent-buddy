77fcf60e0dff65351f2c7169c875bbf75a03141a
Charlotte Mountain <charr.mt@gmail.com>
2020-05-21 12:34:40 -0700

    close with delete

--- client/src/pages/ProfileWidgets/WidgetBumper.js
+++ client/src/pages/ProfileWidgets/WidgetBumper.js
@@ -37,7 +37,7 @@ class WidgetBumper extends Component {
             <p>Are you sure you want to delete your widget?</p>
           </Modal.Content>
           <Modal.Actions>
-            <Button style={{fontFamily:"Roboto"}} onClick={this.props.handleDeleteWidget} inverted color="red">
+            <Button style={{fontFamily:"Roboto"}} onClick={this.props.handleDeleteWidget} onClose={this.close} inverted color="red">
               DELETE
             </Button>
             <Button onClick={this.close} inverted icon>
