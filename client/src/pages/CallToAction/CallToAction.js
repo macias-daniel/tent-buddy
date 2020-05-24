abf8c561b9967f38d154489fbac366fce865e3fc
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-05-23 18:38:37 -0700

    replaced bungee inline with bungee

--- client/src/pages/CallToAction/CallToAction.js
+++ client/src/pages/CallToAction/CallToAction.js
@@ -9,7 +9,7 @@ function CallToAction () {
 
         < Container className="intro">
           <Segment centered raised compact inverted className="intro2" style={{ marginBottom: "0", background: "rgba(33, 187, 128, 0.78)", width: "85%", margin: "0 auto", marginTop: "1.5rem", color:"#F69C3C" }}>
-            <h2 style={{fontFamily:"Bungee Inline", color:"white"}} className="introHead">Why Tent Buddy?</h2>
+            <h2 style={{fontFamily:"Bungee", color:"white"}} className="introHead">Why Tent Buddy?</h2>
           </Segment>
           <Segment centered raised compact inverted className="intro3" style={{ marginTop: "0", width: "85%", margin: "0 auto", background:"#1b1c1d"}}>
             <h4 style={{fontFamily:"Roboto"}} >Have you ever wanted a convenient place to look at important information needed for a smooth outdoors expedition? 
@@ -22,7 +22,7 @@ function CallToAction () {
 
         <Container className="features">
           <Segment centered raised compact inverted className="intro2" style={{ marginBottom: "0", background: "rgba(33, 187, 128, 0.78)", width:"100%" , margin: "0 auto", color:"#F69C3C" }}>
-            <h2 style={{fontFamily:"Bungee Inline", color:"white"}} >Key Features</h2>
+            <h2 style={{fontFamily:"Bungee", color:"white"}} >Key Features</h2>
           </Segment>
           <Segment centered raised compact inverted className="intro3" style={{ marginTop: "0" , background:"#1b1c1d", width:"100%", margin: "0 auto" }}>
             <List bulleted style={{color: "white", fontSize: "1.25rem"}}>
@@ -40,7 +40,7 @@ function CallToAction () {
 
         <Container className="mission">
           <Segment centered raised compact inverted className="intro2" style={{ marginBottom: "0", background: "rgba(33, 187, 128, 0.78)", width: "85%", margin: "0 auto", color:"#F69C3C" }}>
-            <h2 style={{fontFamily:"Bungee Inline", color:"white"}} >Our Mission</h2>
+            <h2 style={{fontFamily:"Bungee", color:"white"}} >Our Mission</h2>
           </Segment>
           <Segment centered raised compact inverted className="intro3" style={{ marginTop: "0", width: "85%", margin: "0 auto", background:"#1b1c1d" }}>
             <h4 style={{fontFamily:"Roboto"}} >Tent Buddy was developed to provide individuals with the ability to keep track of a variety of widgets when embarking on an adventure in the great outdoors. </h4>
