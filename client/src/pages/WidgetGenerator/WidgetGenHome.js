a372e3a8296dd172cdc2e409aa49fd4bd23578bc
Charlotte Mountain <charr.mt@gmail.com>
2020-06-01 09:35:59 -0700

    test

--- client/src/pages/WidgetGenerator/WidgetGenHome.js
+++ client/src/pages/WidgetGenerator/WidgetGenHome.js
@@ -5,37 +5,37 @@ import WidgetGeneratorSort from "./WidgetGenSort";
 function WidgetHome() {
   return (
     <Grid style={{ margin: "0px", padding: "0px" }} centered>
-        <Container className="profileContainer">
-      <Container
-        className="widgetGenHome"
-        style={{
-          backgroundColor: "rgba(33, 187, 128, 0)",
-          marginBottom: "0px",
-        }}
-      >
-        <h2
+      <Container className="profileContainer">
+        <Container
+          className="widgetGenHome"
           style={{
+            backgroundColor: "rgba(33, 187, 128, 0)",
             marginBottom: "0px",
-            padding: "0px",
-            fontFamily: "Bungee",
-            color: "white",
           }}
         >
-          Widget Generator
-        </h2>
-        <p
-          style={{
-            margin: "0px",
-            padding: "0px",
-            fontFamily: "Bungee",
-            color: "white",
-          }}
-        >
-          pick a category
-        </p>
-      </Container>
+          <h2
+            style={{
+              marginBottom: "0px",
+              padding: "0px",
+              fontFamily: "Bungee",
+              color: "white",
+            }}
+          >
+            Widget Generator
+          </h2>
+          <p
+            style={{
+              margin: "0px",
+              padding: "0px",
+              fontFamily: "Bungee",
+              color: "white",
+            }}
+          >
+            pick a category
+          </p>
+        </Container>
 
-      <WidgetGeneratorSort />
+        <WidgetGeneratorSort />
       </Container>
     </Grid>
   );
